fe5d4f922ee9f232660bb0b9a0141ff2475961c9
Ugur Artun <artun.ugur@gmail.com>
2019-12-13 23:08:28 +0300

    sum function

--- src/firstapp.kt
+++ src/firstapp.kt
@@ -4,8 +4,21 @@ fun main(args : Array<String>) {
     println("Hello World")
     var sum = sum(3,5)
     println(sum)
+    operator()
 }
 
 fun sum(a: Int, b : Int): Int {
     return a+b
+}
+
+fun operator() {
+    var a = 20
+    var b = 4
+    println("a + b = " + a.plus(b))
+}
+
+fun conversion() {
+    var myLongNumber = 10L
+    var myNumber: Int = myLongNumber.toInt()
+    println(myNumber)
 }
