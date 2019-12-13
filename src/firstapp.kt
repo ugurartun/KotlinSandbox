f81fc0be031ad508ca362bbf98148246ef83693d
Ugur Artun <artun.ugur@gmail.com>
2019-12-13 21:26:31 +0300

    sum function

--- src/firstapp.kt
+++ src/firstapp.kt
@@ -1,6 +1,11 @@
-// first method in Kotlin
+
 fun main(args : Array<String>) {
+    val myName = "Gaurav"
     println("Hello World")
-    println("Kotlin")
+    var sum = sum(3,5)
+    println(sum)
 }
-|
+
+fun sum(a: Int, b : Int): Int {
+    return a+b
+}
