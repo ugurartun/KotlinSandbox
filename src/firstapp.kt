5694ba62127d8200b7e2dcaf01c0f64140ca92b6
Ugur Artun <artun.ugur@gmail.com>
2019-12-15 00:56:40 +0300

    for loop function

--- src/firstapp.kt
+++ src/firstapp.kt
@@ -48,4 +48,21 @@ fun forLoop() {
     for (i in 1..10 step 3) {
         print("$i ")
     }
+    for (i in 5 downTo 1) {
+        print("$i ")
+    }
+
+    var name = "Geeks"
+    var name2 = "forGeeks"
+
+    // traversing string without using index property
+    for (alphabet in name)   print("$alphabet ")
+
+    // traversing string with using index property
+    for (i in name2.indices) print(name2[i]+" ")
+    println(" ")
+
+    // traversing string using withIndex() library function
+    for ((index,value) in name.withIndex())
+        println("Element at $index th index is $value")
 }
