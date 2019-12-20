0b0a876e434defedec70fe342c559b3d1dc7f0fe
Uğur Artun <ugur.artun@sahibinden.com>
2019-12-20 13:23:29 +0300

    enum classes

--- src/Day.kt
+++ src/Day.kt
@@ -1,9 +1,9 @@
-enum class Day {
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY
+enum class Day (val dayStr : String){
+    SUNDAY("sunday"),
+    MONDAY("monday"),
+    TUESDAY("tuesday"),
+    WEDNESDAY("wednesday"),
+    THURSDAY("thursday"),
+    FRIDAY("friday"),
+    SATURDAY("saturday")
 }
